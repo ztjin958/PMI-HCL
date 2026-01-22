a0237084963038e15fab5ccdea16f1d220853cb6
ztjin958 <ztjin958@gmail.com>
2026-01-22 13:42:32 +0800

    Update Prepare.py

--- Prepare.py
+++ Prepare.py
@@ -8,7 +8,7 @@ np.random.seed(42)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 cs = "_400"
-path = r'path of your dataset'
+path = r'PMI-HCL/stitch_ecoil' # path of your dataset
 
 df = pd.read_csv(os.path.join(path,f"m_p_links{cs}.csv"))
 # df = pd.read_csv(os.path.join(path,f"m_p_links.csv"))
@@ -171,6 +171,7 @@ specificity_scores = []
 precision_score_scores = []
 print(f"meta_nums:{len(df_pmi_meta_list)}  protein_nums:{len(df_pmi_protein_list)}")
 print("Prepare is done")
+
 
 
 
